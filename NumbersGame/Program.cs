f39a3bf1534881c270333953ef60fdd9ffb253ed
Ben Letbetter <ben.letbetter@outlook.com>
2020-03-10 13:50:30 -0400

    Input Validation1

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -8,41 +8,42 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
-            int? input;
-            input = null;
+            int input;
             do
             {
-                if(input != null)
+                //if(input != null)
+                //{
+                //    Console.WriteLine("Press any key to continue...");
+                //    Console.ReadKey();
+                //}
+                MainMenu();
+                var val = Console.ReadLine();
+                while (ValidOption(val, out input)==false)
                 {
-                    Console.WriteLine("Press any key to continue...");
-                    Console.Read();
+                    MainMenu();
+                    val = Console.ReadLine();
                 }
-                Console.Clear();
-                Console.WriteLine("Welcome to the number guessing game.");
-                Console.WriteLine("Can you beat the computer in attempts to guess a random number?");
-                Console.WriteLine("Choose one of the following:");
-                Console.WriteLine("1.) See the computers thought process \n2.) Guess a number yourself \n3.) Have the computer guess your number \n4.) Exit");
                 
-                Console.Write("You choose: ");
-                input = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Press any key to continue...");
-                Console.Read();
                 switch (input)
                 {
                     case 1:
                         Console.Clear();
                         Run();
-                       
+
+                        Console.WriteLine("Press any key to continue...");
+                        Console.Read();
                         break;
                     case 2:
                         Console.Clear();
                         HumanGuess();
-                        
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
                         break;
                     case 3:
                         Console.Clear();
                         ComputerGuess();
-                        
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
                         break;
                     case 4:
                         Console.WriteLine("\n\n\nThanks for playing!");
@@ -54,8 +55,9 @@ namespace NumbersGame
                         break;
 
                 }
-
+                
             } while (input != 4);
+            
             static void HumanGuess()
             {
                 int input;
@@ -70,8 +72,12 @@ namespace NumbersGame
                 int f;
                 for (f = 11; f > 0; --f)
                 {
-                    Console.WriteLine("\n \nYour guess: ");
-                    input = Int32.Parse(Console.ReadLine());
+                    var val = Console.ReadLine();
+                    while (ValidOption(val, out input) == false)
+                    {
+                        Console.WriteLine("\n \nYour guess: ");
+                        input = Int32.Parse(Console.ReadLine());
+                    }
                     if (input == rndNum)
                     {
                         break;
@@ -108,15 +114,18 @@ namespace NumbersGame
                 int rndNum, input, value, attempts;
                 value = list.Length / 2;
                 attempts = 0;
-                Console.WriteLine("For this, you will input a number for the computer to guess btween 1 - 100.");
+                Console.WriteLine("For this, you will input a number for the computer to guess between 1 - 100.");
                 Console.WriteLine("You will then tell the computer if it's guess is higher or lower then the number you entered.");
                 do
                 {
+                    Console.WriteLine("Enter a number between 1 - 100");
                     Console.WriteLine("Your number to be guessed is: ");
-                    rndNum = Int32.Parse(Console.ReadLine());
-                    if (rndNum > 100 || rndNum < 1)
+                    var val = Console.ReadLine();
+                    while (ValidOption(val, out rndNum) == false)
                     {
-                        Console.WriteLine("number entered is outside needed field. \n \n Try again. \n");
+                        Console.WriteLine("Enter a number between 1 - 100");
+                        Console.WriteLine("Your number to be guessed is: ");
+                        val = Console.ReadLine();
                     }
                 } while (rndNum < 1 && rndNum > 100);
                 while (rndNum != list[value])
@@ -131,8 +140,19 @@ namespace NumbersGame
                     Console.WriteLine($"Computer attempt {attempts}");
                     Console.WriteLine($"\nThe computer's guess is {list[value]} \n \n");
                     Console.WriteLine("Enter \n1.) if the guess is higher than your number. \n2.) if the guess is lower than your number.");
-                    Console.WriteLine("Your responce is: ");
-                    input = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("3.) if the guess was correct");
+                    do
+                    {
+                        Console.WriteLine("Enter a number between 1 - 3");
+                        Console.WriteLine("Your responce is: ");
+                        var val = Console.ReadLine();
+                        while (ValidOption(val, out input) == false)
+                        {
+                            Console.WriteLine("Enter a number between 1 - 3");
+                            Console.WriteLine("Your responce is: ");
+                            val = Console.ReadLine();
+                        }
+                    } while (input < 1 && input > 3);
                     switch (input)
                     {
                         case 1:
@@ -168,8 +188,24 @@ namespace NumbersGame
             static void Run()
             {
                 int[] list = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-                int value = 7;
-                Console.WriteLine("The number to find is 7");
+                int value;
+                Console.WriteLine("Enter a number 1 - 10");
+                var val = Console.ReadLine();
+                while (ValidOption(val, out value) == false)
+                {
+                    Console.WriteLine("Enter a number 1 - 10");
+                    val = Console.ReadLine();
+                }
+                while (value > 10 || value < 1)
+                {
+                    Console.WriteLine("Enter a number 1 - 10");
+                    while (ValidOption(val, out value) == false)
+                    {
+                        Console.WriteLine("Enter a number 1 - 10");
+                        val = Console.ReadLine();
+                    }
+                }
+                Console.WriteLine($"The number to find is {list[value]}");
                 FindNumber(list, value);
                 
 
@@ -212,6 +248,20 @@ namespace NumbersGame
                     Console.WriteLine($"\nThe computer guesses the number is {list[middle]}");
                 }
                 Console.WriteLine($"The computer guessed the correct number!");
+            }
+            static bool ValidOption(string input, out int option)
+            {
+                return Int32.TryParse(input, out option);
+            }
+            static void MainMenu()
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the number guessing game.");
+                Console.WriteLine("Can you beat the computer in attempts to guess a random number?");
+                Console.WriteLine("Choose one of the following:");
+                Console.WriteLine("1.) See the computers thought process \n2.) Guess a number yourself \n3.) Have the computer guess your number \n4.) Exit");
+
+                Console.Write("You choose: ");
             }
         }
     }
